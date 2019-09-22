ab01c9502a7268351c3681a10dc057c9eb108e96
unknown <j.ed.glossop@gmail.com>
2019-09-22 12:45:55 +0100

    Initial game mechanics implemented.

--- Assets/Scenes/Game.gd
+++ Assets/Scenes/Game.gd
@@ -1,9 +1,35 @@
 extends Node
 
 var playing = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var blocks = preload("res://Assets/Tower/tower_blocks.tscn").instance()
+var new_block
+var should_lock_block = false
+
+var ortho_bases = [
+	Basis(Vector3( 1,  0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1)),
+	Basis(Vector3( 0, -1, 0), Vector3(1, 0, 0), Vector3(0, 0, 1)),
+	Basis(Vector3(-1,  0, 0), Vector3(0, -1, 0), Vector3(0, 0, 1)),
+	Basis(Vector3( 0,  1, 0), Vector3(-1, 0, 0), Vector3(0, 0, 1)),
+	Basis(Vector3( 1,  0, 0), Vector3(0, 0, -1), Vector3(0, 1, 0)),
+	Basis(Vector3( 0,  0, 1), Vector3(1, 0, 0), Vector3(0, 1, 0)),
+	Basis(Vector3(-1,  0, 0), Vector3(0, 0, 1), Vector3(0, 1, 0)),
+	Basis(Vector3( 0,  0, -1), Vector3(-1, 0, 0), Vector3(0, 1, 0)),
+	Basis(Vector3( 1,  0, 0), Vector3(0, -1, 0), Vector3(0, 0, -1)),
+	Basis(Vector3( 0,  1, 0), Vector3(1, 0, 0), Vector3(0, 0, -1)),
+	Basis(Vector3(-1,  0, 0), Vector3(0, 1, 0), Vector3(0, 0, -1)),
+	Basis(Vector3( 0, -1, 0), Vector3(-1, 0, 0), Vector3(0, 0, -1)),
+	Basis(Vector3( 1,  0, 0), Vector3(0, 0, 1), Vector3(0, -1, 0)),
+	Basis(Vector3( 0,  0, -1), Vector3(1, 0, 0), Vector3(0, -1, 0)),
+	Basis(Vector3(-1,  0, 0), Vector3(0, 0, -1), Vector3(0, -1, 0)),
+	Basis(Vector3( 0,  0, 1), Vector3(-1, 0, 0), Vector3(0, -1, 0)),
+	Basis(Vector3( 0,  0, 1), Vector3(0, 1, 0), Vector3(-1, 0, 0)),
+	Basis(Vector3( 0, -1, 0), Vector3(0, 0, 1), Vector3(-1, 0, 0)),
+	Basis(Vector3( 0,  0, -1), Vector3(0, -1, 0), Vector3(-1, 0, 0)),
+	Basis(Vector3( 0,  1, 0), Vector3(0, 0, -1), Vector3(-1, 0, 0)),
+	Basis(Vector3( 0,  0, 1), Vector3(0, -1, 0), Vector3(1, 0, 0)),
+	Basis(Vector3( 0,  1, 0), Vector3(0, 0, 1), Vector3(1, 0, 0)),
+	Basis(Vector3( 0,  0, -1), Vector3(0, 1, 0), Vector3(1, 0, 0)),
+	Basis(Vector3( 0, -1, 0), Vector3(0, 0, -1), Vector3(1, 0, 0))]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,6 +54,23 @@ func _input(event):
 			playing = true
 			find_node("Pause Panel").hide()
 			get_tree().paused = false	
+			
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_F5:
+			get_tree().reload_current_scene()
+	
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_LEFT:
+	
+			if rand_range(0, 1) > 0.5:
+				new_block = blocks.get_node("Bent_Block").duplicate()
+			else:
+				new_block = blocks.get_node("Straight_Block").duplicate()
+				
+			add_child(new_block)
+			new_block.set_identity()
+			new_block.global_translate(Vector3(0, 20, rand_range(-6, 6)))
+			new_block.connect("sleeping_state_changed", self, "_block_is_sleeping")
 
 func _on_Game_Started():
 	print("Game started!")
@@ -36,3 +79,17 @@ func _on_Game_Started():
 func _on_Game_Ended():
 	print("Gamed!")
 	playing = false
+	
+func _block_is_sleeping():
+	should_lock_block = true
+	
+func _physics_process(delta):
+	if should_lock_block:
+		should_lock_block = false
+
+		var rotation = Basis(Vector3(1,0,0), 1.5708 * floor((new_block.rotation_degrees.x + 45)/90))
+		var y = int(new_block.transform.origin.y + .5) + int(new_block.transform.origin.y + .5) % 2 - 1
+		var z = int(new_block.transform.origin.z + .5) + int(new_block.transform.origin.z + .5) % 2
+		
+		new_block.set_mode(RigidBody.MODE_STATIC)
+		new_block.set_transform(Transform(rotation, Vector3(0, y, z)))
